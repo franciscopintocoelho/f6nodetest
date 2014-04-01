22bffdacbd9ce52c0df4180b66040afab802fcb4
= <=>
2014-04-01 16:59:38 +0100

    test port

--- server.js
+++ server.js
@@ -4,15 +4,3 @@ http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World' + process.env.port + '\n');
 }).listen(port);
-
-var io = require('socket.io').listen(process.env.port);
-
-io.sockets.on('connection', function (socket) {
-  	socket.on('speak', function (data) {
-    	io.sockets.emit('speak', data);
-  	});
-  	
-  	socket.on('disconnect', function () {
-    	io.sockets.emit('user disconnected');
-  	});
-});
