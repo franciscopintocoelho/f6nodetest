8716af96224f98a2e0518e52da2de4966bd775bf
= <=>
2014-04-01 16:49:53 +0100

    added: socket.io server

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+// note, io.listen(<port>) will create a http server for you
+var io = require('socket.io').listen(80);
+
+io.sockets.on('connection', function (socket) {
+  	socket.on('speak', function (data) {
+    	io.sockets.emit('speak', data);
+  	});
+  	
+  	socket.on('disconnect', function () {
+    	io.sockets.emit('user disconnected');
+  	});
+});
