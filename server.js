fe4bfc95d4bcf3a369e3a998cfc08337e83497bd
= <=>
2014-04-01 17:07:33 +0100

    added: port

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-var http = require('http');
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World' + process.env.port + '\n');
-}).listen(port);
+var port = process.env.port || 1337;
+var io = require('socket.io').listen(port);
+
+io.sockets.on('connection', function (socket) {
+  	socket.on('speak', function (data) {
+    	io.sockets.emit('speak', data);
+  	});
+  	
+  	socket.on('disconnect', function () {
+    	io.sockets.emit('user disconnected');
+  	});
+});
