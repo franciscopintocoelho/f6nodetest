e67499c15592e24d5d59042d7e6765b76a753ad9
= <=>
2014-04-01 16:52:55 +0100

    test

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
-// note, io.listen(<port>) will create a http server for you
-var io = require('socket.io').listen(80);
-
-io.sockets.on('connection', function (socket) {
-  	socket.on('speak', function (data) {
-    	io.sockets.emit('speak', data);
-  	});
-  	
-  	socket.on('disconnect', function () {
-    	io.sockets.emit('user disconnected');
-  	});
-});
+var http = require('http');
+var io = require('socket.io');
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
