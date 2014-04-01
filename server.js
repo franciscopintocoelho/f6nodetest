118458e7941ab1d44f136ecf8aaa929abc646d83
= <=>
2014-04-01 16:54:23 +0100

    added: IO sockets

--- server.js
+++ server.js
@@ -5,3 +5,15 @@ http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
 }).listen(port);
+
+io.listen(8080);
+
+io.sockets.on('connection', function (socket) {
+  	socket.on('speak', function (data) {
+    	io.sockets.emit('speak', data);
+  	});
+  	
+  	socket.on('disconnect', function () {
+    	io.sockets.emit('user disconnected');
+  	});
+});
