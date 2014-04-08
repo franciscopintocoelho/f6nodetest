26a53a0202d6000edea4957afebf58085eeab367
= <=>
2014-04-08 16:03:29 +0100

    changed: server

--- server.js
+++ server.js
@@ -1,10 +1,1 @@
-var port = process.env.port || 81;
-
-var app = require('http').createServer(function(req, res) {
-	fs.readFile('index.html', 'utf8', function (errors, contents) {
-		res.writeHead(200);
-		response.end(contents); 
-	});
-});
-
-app.listen(port);
+var port = process.env.port || 8080;
