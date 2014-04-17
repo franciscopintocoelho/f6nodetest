2f01036857be59e4c16537bea6d15256fc456493
= <=>
2014-04-17 18:30:41 +0100

    added: server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+var port = process.env.port || 8080;
+
+var app = require('http').createServer(handler),
+	io = require('socket.io').listen(app)
+
+app.listen(port);
+console.log('socket.io server started on port: ' + port + '\n');
+
+function handler (req, res) {
+	res.writeHead(200);
+	res.end('socket.io server started on port: ' + port + '\n');
+}
+
+io.sockets.on('connection', function (socket) {
+	console.log('connected');
+	
+	socket.on('location', function (data) {
+		io.sockets.emit('location', data);
+	});
+
+	socket.on('disconnect', function () {
+		console.log('disconnected');
+		io.sockets.emit('user disconnected');
+	});
+});
